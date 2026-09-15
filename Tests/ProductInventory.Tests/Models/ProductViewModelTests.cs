68250288078aa2028458cdcca1c6241bdc40394d
jdepling <30660621+jdepling@users.noreply.github.com>
2026-09-15 13:49:50 -0600

    Add tests to test the quantity. And a helper to utilize the validation context

--- Tests/ProductInventory.Tests/Models/ProductViewModelTests.cs
+++ Tests/ProductInventory.Tests/Models/ProductViewModelTests.cs
@@ -1,4 +1,6 @@
-﻿using ProductInventory.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using ProductInventory.Models;
+using ValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
 
 namespace ProductInventory.Tests.Models
 {
@@ -20,9 +22,31 @@ namespace ProductInventory.Tests.Models
         }
 
         [Fact]
-        public void Bad()
+        public void Quantity_NegativeQuantity_FailsValidation()
         {
-            Assert.Fail("This test is intentionally failing to demonstrate a failing test case.");
+            // Arrange
+            var product = new ProductViewModel { Quantity = -1 };
+
+            // Act
+            var result = ValidateModel(product);
+            var errorMessageForQuantity = result
+                .Where(r => r.MemberNames.Contains(nameof(ProductViewModel.Quantity)))
+                .Select(r => r.ErrorMessage)
+                .FirstOrDefault();
+
+            // Assert
+            Assert.Equal("Quantity cannot be negative", errorMessageForQuantity);
         }
+
+        private IList<ValidationResult> ValidateModel(object model)
+        {
+            var validationResults = new List<ValidationResult>();
+            var ctx = new ValidationContext(model, null, null);
+            Validator.TryValidateObject(model, ctx, validationResults, true);
+            return validationResults;
+        }
+
     }
 }
+
+       
